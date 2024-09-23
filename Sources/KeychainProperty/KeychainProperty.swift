af05b04787e4e241f1bd130805d1740ee0cf540f
franzlj <1780720+franzlj@users.noreply.github.com>
2024-09-23 15:00:41 +0200

    Fix error handling

--- Sources/KeychainProperty/KeychainProperty.swift
+++ Sources/KeychainProperty/KeychainProperty.swift
@@ -96,7 +96,7 @@ public struct KeychainProperty<T: Codable> {
             if let newValue, let data = try? encoder.encode(newValue) {
                 keychain[data: valueKey] = data
             } else {
-                keychain.remove(valueKey)
+                try? keychain.remove(valueKey)
             }
             
             valueSubject.send(newValue)
